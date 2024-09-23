84960db9762fa8e5f2a4af7fdcbe0fad149d28e0
samundra shrestha <samundras41@gmail.com>
2024-09-23 23:46:18 +0545

    composer added, autoloader added

--- public/index.php
+++ public/index.php
@@ -3,22 +3,24 @@
 use Core\Router;
 use Core\Session;
 use Core\ValidationException;
-session_start();
+
 
 const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . 'vendor/autoload.php';
 
+session_start();
 // var_dump(BASE_PATH);
 
 require BASE_PATH . 'Core/functions.php';
 
 
-spl_autoload_register(function ($class) {
-    // var_dump(base_path($class . '.php'));
+// spl_autoload_register(function ($class) {
+//     // var_dump(base_path($class . '.php'));
 
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+//     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
-    require base_path("{$class}.php");
-});
+//     require base_path("{$class}.php");
+// });
 
 require base_path('bootstrap.php');
 
