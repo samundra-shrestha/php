1b2797db79e78e57c15891f3bb5f95a68b4b0bd8
samundra shrestha <samundras41@gmail.com>
2024-09-26 23:32:56 +0545

    auth implemented for notes create, edit, delete, view

--- routes.php
+++ routes.php
@@ -10,11 +10,11 @@ $router->get('/notes', 'notes/index.php')->only('auth');
 $router->get('/note', 'notes/show.php')->only('auth');
 $router->delete('/note', 'notes/destroy.php')->only('auth');
 
-$router->get('/notes/create', 'notes/create.php');
-$router->post('/notes', 'notes/store.php');
+$router->get('/notes/create', 'notes/create.php')->only('auth');
+$router->post('/notes', 'notes/store.php')->only('auth');
 
-$router->get('/note/edit', 'notes/edit.php');
-$router->patch('/note', 'notes/update.php');
+$router->get('/note/edit', 'notes/edit.php')->only('auth');
+$router->patch('/note', 'notes/update.php')->only('auth');
 
 $router->get('/register', 'registration/create.php')->only('guest');
 $router->post('/register', 'registration/store.php')->only('guest');
